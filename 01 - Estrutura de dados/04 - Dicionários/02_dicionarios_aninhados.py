a7ca038bd7ab415d3f932a2293934f970cf7a510
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-11 15:42:22 -0300

    up

--- 01 - Estrutura de dados/04 - Dicionários/02_dicionarios_aninhados.py
+++ 01 - Estrutura de dados/04 - Dicionários/02_dicionarios_aninhados.py
@@ -3,7 +3,10 @@ contatos = {
     "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
     "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
     "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+    "mandrejezieski@gmail.com": {"nome": "Misa", "telefone": "(42)991378801", "Data nasc": "13/09/1982"},
 }
 
-telefone = contatos["giovanna@gmail.com"]["telefone"]  # "3443-2121"
+telefone = contatos["mandrejezieski@gmail.com"]["telefone"]  # "(42)991378801"
 print(telefone)
+data_nasc = contatos["mandrejezieski@gmail.com"]["Data nasc"] # '13/09/1982"
+print(data_nasc)
