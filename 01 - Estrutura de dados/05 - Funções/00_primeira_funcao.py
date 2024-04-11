fcd24894862ad7e4f8b31efb7991d8617827332a
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-11 19:06:54 -0300

    Update 00_primeira_funcao.py

--- 01 - Estrutura de dados/05 - Funções/00_primeira_funcao.py
+++ 01 - Estrutura de dados/05 - Funções/00_primeira_funcao.py
@@ -11,7 +11,12 @@ def exibir_mensagem_2(nome):
 def exibir_mensagem_3(nome="Anônimo"):
     print(f"Seja bem vindo {nome}!")
 
+def saudacao():
+    nome = input("Por favor, insira seu nome: ")
+    print(f"Oi, {nome}!")
 
+# Para usar a função, basta chamar:
+saudacao()
 exibir_mensagem()
 exibir_mensagem_2(nome="Guilherme")
 exibir_mensagem_3()
